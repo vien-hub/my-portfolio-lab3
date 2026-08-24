f188620e9bc95a9f0bb0105d8825d4602d638462
vien-hub <vienmira1@gmail.com>
2026-08-24 08:22:55 +0800

    Add some info in about

--- my-portfolio/src/pages/About.jsx
+++ my-portfolio/src/pages/About.jsx
@@ -2,11 +2,12 @@ export default function About() {
     return (
       <div>
         <h1>About Me</h1>
-        <p>I specialize in React, JavaScript, and modern Web Development.</p>
+        <p>I specialize in React, JavaScript, and modern Web Development with Some knowlegde about Java.</p>
         <h3>Skills</h3>
         <ul>
           <li>React & React Router</li>
-          <li>JavaScript (ES6+)</li>
+          <li>Java</li>
+          <li>JavaScript </li>
           <li>HTML5 / CSS3</li>
           <li>Git & GitHub</li>
         </ul>
